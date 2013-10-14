fb588d57ce6623d58f8e9622531e7a1bafff70ba
Chris Leaf <chris@thinkero.us>
2013-10-14 14:37:23 -0400

    Updated NavigateRouter

--- NavigateRouter.js
+++ NavigateRouter.js
@@ -6,6 +6,10 @@ define([
 ], function(Backbone,commands){
   return Backbone.Router.extend({
     history: [],
+    routes: {
+      '_=_': "facebookLogin",
+      '*actions': "defaultRoute"
+    }, 
     initialize: function(options){
       this.commands = commands; 
       this.commands.setHandler("navigateHome", this.goHome, this);
@@ -16,7 +20,24 @@ define([
       this.commands.setHandler("urlAppend", this.append, this);
       this.commands.setHandler("urlInsert", this.insert, this);
       this.commands.setHandler("urlTruncate", this.truncate, this);
+
+      $(document).on("click", "a[href^='/']", function(event) {
+        var href, passThrough, url;
+        href = $(event.currentTarget).attr('href');
+        passThrough = href.indexOf('sign_out') >= 0;
+      });
+
     },
+    facebookLogin: function(){
+      this.goToPage('user/edit');
+    },
+    defaultRoute: function(actions){
+      console.log('Caught default Route: ' + actions);
+      this.goToPage(actions);
+    },
+    getUrl: function(){
+      return window.location.pathname.substr(1) + window.location.search;
+    }, 
     cleanRoute: function(route){
       return _.map(route.split('/'), function(frag){
         return  frag.replace(/\s+/g, '-')
@@ -36,7 +57,7 @@ define([
       }, options);
       
       if(options.pushUrl){
-        this.history.push(Backbone.history.fragment);
+        this.history.push(this.getUrl());
       }
       if(options.popUrl){
         if(this.history.length <= 0) throw "No history left to pop"; 
@@ -61,16 +82,19 @@ define([
         trigger: false, 
         replace: true,
         pushUrl: false,
-        popUrl: false
+        popUrl: false, 
+        useCurrent: false
       }, options);
 
       if(options.pushUrl){
-        this.history.push(Backbone.history.fragment);
+        this.history.push(this.getUrl());
       }
       if(options.popUrl){
         if(this.history.length <= 0) throw "No history left to pop"; 
         route = this.history.pop();
       }
+      if(options.useCurrent)
+        route = this.getUrl(); 
       route = this.cleanRoute(route);
       this.navigate(route, options);
       if(typeof options.success === 'function'){
@@ -84,18 +108,36 @@ define([
       this.urlUpdate("", options); 
     }, 
     truncate: function(options){
-      var route = Backbone.history.fragment.split('/');
+      var route = this.getUrl();
       if(typeof options.by !== 'undefined'){
+        route = route.split('/');
         route = route.slice(0,route.length - options.by);
+        route = route.join('/');
       }else if(typeof options.to !== 'undefined'){
+        route = route.split('/');
         route = route.slice(0,options.to);
+        route = route.join('/');
+      }else if(typeof options.from !== 'undefined'){
+        var index = route.indexOf(options.from);
+        if(index > 0)
+          route = route.substr(0, index);
       }
-      route = route.join('/');
 
       this.urlUpdate(route, options);
     },
     append: function(suffix, options){
-      this.urlUpdate(Backbone.history.fragment + "/" + suffix, options);
+      options = _.extend({query: false}, options);
+      if(options.query){
+        if(this.getUrl().indexOf("?") > -1) {
+          this.urlUpdate(this.getUrl() + "&" +  suffix, options);
+        }else{
+          this.urlUpdate(this.getUrl() + "?" +  suffix, options);
+        }
+      }
+      else{
+        this.truncate({from : "?"});
+        this.urlUpdate(Backbone.history.fragment + "/" + suffix, options);
+      }
     },
     insert: function(str, options){
       var t = Backbone.history.fragment;
